2c178f5383546469bdbf88219ddb5d324258247a
Watanuki Takemi <takemi007@hotmail.com>
2017-04-16 02:00:52 +0900

    Initial Commit

---
+++ tKansai0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tKansai0
+//
+//  Created by Takemi Watanuki on 2017/04/16.
+//  Copyright © 2017年 watakemi725. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
